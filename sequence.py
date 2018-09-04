2fe261b51d3bce3a0cd2f145ae1283dab99834a0
olafur-andri <olafur1199@gmail.com>
2018-09-04 14:40:13 +0000

    Algorithm for sequence.py

---
+++ sequence.py
@@ -0,0 +1,10 @@
+# Input an integer "n"
+# Instantiate the integer "a1 = 1", "a2 = 2" and "a3 = 3" which will be tracking changes in the sequence
+# Instantiate the integer "sum_int = 0" which will be the sum of a1, a2 and a3
+#
+# for loop from 1 to n
+#   Make the sum_int equal to a1 + a2 + a3
+#   Make a1 = a2
+#   Make a2 = a3
+#   Make a3 = sum_int
+#   Print out sum_int
