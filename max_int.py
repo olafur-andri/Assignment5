4002e976b5f4aee34c470e8a9b10f9b1831d8b14
olafur-andri <olafur1199@gmail.com>
2018-09-04 14:07:44 +0000

    Algorithm written for max_int

---
+++ max_int.py
@@ -0,0 +1,9 @@
+# Receive an integer input
+# Instantiate a variable "maximum_int" with a value of the input which will keep track of the greatest number
+#
+# While the input is greater or equal to 0
+#
+#   If the input is greater than maximum_int
+#     Make the input the new maximum_int
+#
+# Print out the maximum_int variable
